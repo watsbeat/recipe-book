aa82ed627750d8a6e46a17d003c01ceccaf06bf8
watsbeat <c.watsonbeaton@gmail.com>
2019-10-19 17:33:02 +1000

    passed recipe data with property binding

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -14,6 +14,11 @@ export class RecipeListComponent implements OnInit {
       'A traditional, hearty meal made with red wine making the mince extra delicious.',
       'https://img.taste.com.au/4geSvCZK/w720-h480-cfill-q80/taste/2016/11/beef-lasagne-77009-1.jpeg'
     ),
+    new Recipe(
+      'Rogan Josh',
+      'A famous Kashmiri Indian lamb curry with enticing spices.',
+      'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/full/public/rogan-josh-01.jpg?itok=pX5BAdXT&mtime=1505184085'
+    ),
   ];
 
   constructor() {}
