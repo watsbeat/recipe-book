06feceafaacaa91d59462c768038403b6522c06a
watsbeat <c.watsonbeaton@gmail.com>
2019-10-20 16:22:01 +1000

    added ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
@@ -8,16 +9,28 @@ export class RecipeService {
     new Recipe(
       'Beef Lasagna',
       'A traditional, hearty meal made with red wine making the mince extra delicious.',
-      'https://img.taste.com.au/4geSvCZK/w720-h480-cfill-q80/taste/2016/11/beef-lasagne-77009-1.jpeg'
+      'https://img.taste.com.au/4geSvCZK/w720-h480-cfill-q80/taste/2016/11/beef-lasagne-77009-1.jpeg',
+      [
+        new Ingredient('Lean beef mince', 1),
+        new Ingredient('Brown onion', 1),
+        new Ingredient('Garlic cloves', 2),
+        new Ingredient('Tomato pasta sauce', 1),
+      ]
     ),
     new Recipe(
       'Rogan Josh',
       'A famous Kashmiri Indian lamb curry with enticing spices.',
-      'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/full/public/rogan-josh-01.jpg?itok=pX5BAdXT&mtime=1505184085'
+      'https://www.sbs.com.au/food/sites/sbs.com.au.food/files/styles/full/public/rogan-josh-01.jpg?itok=pX5BAdXT&mtime=1505184085',
+      [
+        new Ingredient('Cubed lamb', 1),
+        new Ingredient('Chilli powder', 1),
+        new Ingredient('Red onions', 3),
+        new Ingredient('Bay leaves', 3),
+      ]
     ),
   ];
 
   getRecipes() {
-      return this.recipes.slice();
+    return this.recipes.slice();
   }
 }
